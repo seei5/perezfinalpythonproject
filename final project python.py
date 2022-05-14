3a3ab58e0d83f1b15478b20db47258f9f8a60223
seei5 <84622764+seei5@users.noreply.github.com>
2022-05-13 20:11:07 -0500

    Add files via upload

--- final project python.py
+++ final project python.py
@@ -4,13 +4,37 @@ import random
 import math
 import pickle
 from tkinter import *
+import os
 
-### Tables
+## Create neccessary files
 
-colors = []
-images = {}
+def createnewfile(filename):
+    try:
+        with open(f'{filename}.txt', 'r') as file:
+            file.read()
+            file.close()
+    except:
+        with open(f'{filename}.txt', 'w+') as file:
+            file.write('a')
+            file.truncate(0)
+            file.close()
+
+createnewfile('colors')
+createnewfile('designs')
+createnewfile('requests')
+createnewfile('ordervar')
+
+with open('ordervar.txt','r') as file:
+    text = file.read()
+    if text == '' or text == ' ':
+        file.close()
+        with open('ordervar.txt', 'w') as file:
+            file.write('0')
+            file.close()
 
 ### Classes Section
+
+# prices: hat = $1.75, shirt = $3.25
 
 class Admin(): ## admin class
     def __init__(self, username):
@@ -19,6 +43,7 @@ class Admin(): ## admin class
 
     def give_name(self, name):
         self.username = name
+        SaveNewInfoToUser(self, self.username)
 
     def get_type(self):
         return self.priv
@@ -30,16 +55,30 @@ class User(): ## member class
     def __init__(self, username, designs = []):
         self.priv = 'member'
         self.username = username
-        self.designs = designs # list of lists (a design is a list that holds color and design, possibly a name.)
+        self.designs = designs # list of lists (a design is a list that holds color and design)
+        self.orders = []
 
     def give_name(self, name):
         self.username = name
+        SaveNewInfoToUser(self, self.username)
 
     def give_new_design(self, design):
         self.designs.append(design)
+        SaveNewInfoToUser(self, self.username)
 
-    def get_design(self):
+    def give_new_order(self, order):
+        self.orders.append(order)
+        SaveNewInfoToUser(self, self.username)
+
+    def remove_order(self, order):
+        self.orders.remove(order)
+        SaveNewInfoToUser(self, self.username)
+
+    def get_designs(self):
         return self.designs
+
+    def get_orders(self):
+        return self.orders
 
     def get_type(self):
         return self.priv
@@ -47,21 +86,24 @@ class User(): ## member class
     def get_name(self):
         return self.username
 
-class Order():
-    def __init__(self, owner, tshirts=0, hats=0, price=0, shirt_types=None, hat_types=None):
-        self.owner = owner
-        self.tshirts = tshirts
-        self.hats = hats
-        self.price = price
-        self.shirt_types = shirt_types
-        self.hat_types = hat_types
-
-    def get_order_info(self):
-        return self.owner, self.tshirts, self.hats, self.price, self.shirt_types, self.hat_types
-
-# Order(person who placed order (str), # of tshirt (int), # of hats (int), price of order (int), type of shirts ordered (list), type of hats ordered (list))
-
 ### Misc Functions Section
+
+def ChangeOrderVar(amt):
+    with open('ordervar.txt', 'r') as file:
+        ordervar = file.read()
+        ordervar = int(ordervar)+amt
+        ordervar = str(ordervar)
+        file.close()
+    with open('ordervar.txt', 'w') as file:
+        file.truncate(0)
+        file.writelines(ordervar)
+        file.close()
+
+def GetOrderVar():
+    with open('ordervar.txt', 'r') as file:
+        ordervar = int(file.read())
+        file.close()
+        return ordervar
 
 def CreateNewUser(name, usertype):
     name = str(name)
@@ -73,18 +115,46 @@ def CreateNewUser(name, usertype):
         user = Admin(name)
         pickle.dump(user, open(name,"wb"))
 
+def SaveNewInfoToUser(user, username):
+    pickle.dump(user, open(username, 'wb'))
+
 def CreateNewUserRequest(username):
     name = str(username)
     file = open('requests.txt', 'a+')
     file.writelines(name+'\n')
 
-def CreateOrderRequest(owner, tshirts=0, hats=0, price=0, shirt_types=None, hat_types=None):
-    pass
+def CreateOrderRequest(user, owner, tshirts=0, hats=0, price=0, shirt_types=None, hat_types=None):
+    ordervar = GetOrderVar()
+    owner = 'Orderer: '+owner
+    tshirts = "Shirts: "+str(tshirts)
+    hats = 'Hats: '+str(hats)
+    price = 'Price: $'+str(price)
+    if shirt_types == None: shirt_types = 'None'
+    else:
+        shirt_types = str(shirt_types)
+        shirt_types = shirt_types.strip(")")
+        shirt_types = shirt_types.strip('(')
+    if hat_types == None: hat_types = 'None'
+    else: hat_types = str(hat_types)
+    elementlist = [owner, tshirts, hats, price, shirt_types, hat_types, 'Status: Pending']
+    createnewfile(f'order{ordervar}')
+    with open(f'order{ordervar}.txt', 'w') as file:
+        for element in elementlist:
+            if element == 'None':
+                pass
+            else:
+                file.writelines(element+'\n')
+    user.give_new_order(f'order{ordervar}')
+    ChangeOrderVar(1)
 
 def LoginUser(name):
     name = str(name)
     user = pickle.load(open(name, "rb"))
     switch_to_logged_in(user.get_type(), user)
+
+def get_user(username):
+    user = pickle.load(open(username, "rb"))
+    return user
 
 #CreateNewUser(name, usertype) -- takes a username and usertype (i.e. 'aidan' and 'admin' which would be username = 'aidan' and usertype = 'admin')
 #CreateNewUserRequest(username) -- takes a username and creates a request that admins can view and accept or deny
@@ -134,6 +204,7 @@ def home_screen():
     quit_button.place(x=150,y=105, anchor=W)
 
     appendtolist([signup_button, login_button, Title])
+
 def login_screen():
     # Tkinter objects
     Title = Label(root, text="Login Menu")
@@ -228,7 +299,7 @@ def logged_in_screen(utype, user):
 
             view_types = ['View Requests', 'View Orders', 'Create Designs', 'Create Bill'] # User requests, orders, create designs, create bills
             view_var = StringVar()
-            view_var.set(view_types[1])
+            view_var.set(view_types[0])
             
             new_view = OptionMenu(root, view_var, *view_types)
             new_view.place(relx=0.5, rely=.45, anchor=CENTER)
@@ -240,6 +311,8 @@ def logged_in_screen(utype, user):
                     show_user_requests()
                 elif new_screen == 'Create Designs':
                     show_create_designs()
+                elif new_screen == 'View Orders':
+                    show_order_requests()
 
             change_scrn = Button(root, text='View', command=view_new)
             change_scrn.place(relx=0.5, rely=0.8, anchor=CENTER)
@@ -253,7 +326,7 @@ def logged_in_screen(utype, user):
         def default():
             reset_screen()
 
-            view_types = ['Create T-Shirt', 'Order T-Shirt', 'View Orders'] # Create T-Shirt, Order T-Shirt, View Orders
+            view_types = ['Create Custom Design', 'Order Items', 'View Orders'] # Create T-Shirt or hat, Order T-Shirt or hat, View Orders
             view_var = StringVar()
             view_var.set(view_types[1])
             
@@ -263,8 +336,12 @@ def logged_in_screen(utype, user):
             def view_new():
                 new_screen = view_var.get()
                 reset_screen()
-                if new_screen == 'Create T-Shirt':
+                if new_screen == 'Create Custom Design':
                     show_tshirt_creator()
+                elif new_screen == 'Order Items':
+                    show_create_orders()
+                elif new_screen == 'View Orders':
+                    show_view_orders()
 
             change_scrn = Button(root, text='View', command=view_new)
             change_scrn.place(relx=0.5, rely=0.8, anchor=CENTER)
@@ -411,7 +488,6 @@ def logged_in_screen(utype, user):
             picture = str(picture)
             picture = picture.strip('\n')
             user.give_new_design([color, picture])
-            print(user.get_design())
 
         create_design_button = Button(root, text='Create design', command=createdesign)
         create_design_button.place(anchor=CENTER, relx=.5, rely=.8)
@@ -419,7 +495,197 @@ def logged_in_screen(utype, user):
         appendtolist([back, tshirtcolor, tshirtdesign, d_label, c_label, create_design_button])
 
     def show_create_orders():
-        pass
+        def placeorder():
+            owner = user.get_name()
+            if 'T-Shirt' in ordervar.get(): # tshirt or hat identifier
+                tshirts = orderamt.get()
+                hats = 0
+                price = 3.25*int(tshirts)
+                shirttype = orderdesignsvar.get(), ordervar.get()[-1]
+                hattype = None
+            else:
+                hats = orderamt.get()
+                tshirts = 0
+                price = 1.75*int(hats)
+                hattype = orderdesignsvar.get()
+                shirttype = None
+            
+            CreateOrderRequest(user, owner, tshirts, hats, price, shirt_types=shirttype, hat_types=hattype)
+
+        back = Button(root, text='Back', command=default)
+        back.place(x=150,y=130, anchor=W)
+        ordermenu_label = Label(root, text='Order Type:')
+        ordermenu_label.place(anchor=CENTER, rely=.25, relx=.25)
+
+        ordertypes = ['T-Shirt S', 'T-Shirt M', 'T-Shirt L', 'Hat']
+        ordervar = StringVar()
+        ordervar.set(ordertypes[0])
+
+        ordermenu = OptionMenu(root, ordervar, *ordertypes)
+        ordermenu.place(anchor=CENTER, rely=.45, relx=.25)
+
+        try: orderdesigns = user.get_designs()
+        except: orderdesigns = ['White, Plain']
+        
+        orderdesignsvar = StringVar()
+        try: orderdesignsvar.set(orderdesigns[0])
+        except: orderdesigns = ['White, Plain']; orderdesignsvar.set(orderdesigns[0])
+
+        orderdesignsmenu = OptionMenu(root, orderdesignsvar, *orderdesigns)
+        orderdesignsmenu.place(anchor=CENTER, rely=.65, relx=.25)
+
+        orderamt_label = Label(root, text='Order Amount:')
+        orderamt_label.place(anchor=CENTER, rely=.25, relx=.75)
+
+        orderamt = Entry(root)
+        orderamt.place(anchor=CENTER, rely=.45, relx=.75)
+
+        orderbut = Button(root, text="Place order", command = placeorder)
+        orderbut.place(anchor=CENTER, y=130, relx=.5)
+
+        appendtolist([back, ordermenu_label, orderamt_label, ordermenu, orderbut, orderamt, orderdesignsmenu])
+
+    def show_view_orders():
+        back = Button(root, text='Back', command=default)
+        back.place(x=150,y=130, anchor=W)
+
+        view_selector_label = Label(root, text='Your orders:')
+        view_selector_label.place(anchor=W, x=5, rely=.2)
+
+        def view_info():
+            filename = selection_var.get()
+            with open(f'{filename}.txt', 'r') as file:
+                box.delete('1.0', END)
+                for line in file:
+                    box.insert(END, line)
+                file.close()
+
+        try: 
+            selections = user.get_orders()
+            selection_var = StringVar()
+            selection_var.set(selections[0])
+
+            selections_menu = OptionMenu(root, selection_var, *selections)
+            selections_menu.place(anchor=W, x=5, rely=.4)
+
+            view_button = Button(root, text="View Contents", command=view_info)
+            view_button.place(anchor=E, relx=.95, rely=.4)
+
+            appendtolist([selections_menu, view_button])
+
+            box = Text(root, height = 4, width=15)
+            box.place(anchor=W, x=5, rely=.75)
+        except:
+            box = Text(root, height = 5, width=15)
+            box.insert(END, "No orders yet, come back when you've ordered something!")
+            box.place(anchor=W, x=5, rely=.6)
+
+        appendtolist([view_selector_label, box, back])
+
+    def show_order_requests():
+        back = Button(root, text='Back', command=default)
+        back.place(x=150,y=130, anchor=W)
+
+        view_selector_label = Label(root, text='Orders:')
+        view_selector_label.place(anchor=W, x=5, rely=.2)
+
+        def view_info():
+            filename = selection_var.get()
+            with open(f'{filename}', 'r') as file:
+                box.delete('1.0', END)
+                for line in file:
+                    box.insert(END, line)
+                file.close()
+
+        def view_info_wVar(boxitem):
+            filename = selection_var.get()
+            with open(f'{filename}', 'r') as file:
+                boxitem.delete('1.0', END)
+                for line in file:
+                    boxitem.insert(END, line)
+                file.close()
+
+        def accept_order():
+            box1 = Text(root, height=3, width=9)
+            box1.place(anchor=CENTER, relx=.25, rely=.425)
+            box2 = Text(root, height=3, width=9)
+            box2.place(anchor=CENTER, relx=.75, rely=.425)
+            back = Button(root, text='Back', command=default)
+            back.place(x=150,y=130, anchor=W)
+            view_info_wVar(box1)
+            labelbox1 = Label(root, text='Info')
+            labelbox1.place(anchor=CENTER, relx=.25, rely=.2)
+            labelbox2 = Label(root, text='Make bill')
+            labelbox2.place(anchor=CENTER, relx=.75, rely=.2)
+
+            order = selection_var.get()
+            def modify_order():
+                with open(order, 'w') as file:
+                    file.truncate(0)
+                    text=box2.get("1.0",END)
+                    file.write(text)
+                    file.close()
+            confirm_button = Button(root, text="Confirm", command=modify_order)
+            confirm_button.place(anchor=W, x=5, rely=.75)
+            reset_screen()
+            appendtolist([box1, box2, back, labelbox1, labelbox2, confirm_button])
+
+        def deny_order():
+            order = selection_var.get()
+            with open(order, 'r') as file:
+                for line in file:
+                    if 'Orderer' in line:
+                        username = line[9:-1]
+                file.close()
+            user=get_user(username)
+            order = order.strip('.txt')
+            user.remove_order(order)
+            os.remove(f'{order}.txt')
+            ChangeOrderVar(-1)
+            selections.remove(f'{order}.txt')
+            box.delete('1.0', END)
+
+        def check_accepted(order):
+            with open(order, 'r') as file:
+                for line in file:
+                    if 'accepted' in line.lower():
+                        return False
+                return True
+
+        try: 
+            selections = []
+            files = os.listdir('.')
+            for file in files:
+                if '.txt' in file:
+                    if 'order' in file:
+                        if file != 'ordervar.txt':
+                            if check_accepted(file):
+                                selections.append(file)
+            selection_var = StringVar()
+            selection_var.set(selections[0])
+
+            selections_menu = OptionMenu(root, selection_var, *selections)
+            selections_menu.place(anchor=W, x=5, rely=.4)
+
+            view_button = Button(root, text="View", command=view_info)
+            view_button.place(anchor=W, relx=.5, rely=.4)
+
+            accept_button = Button(root, text="Accept", command=accept_order)
+            accept_button.place(anchor=W, x=150,y=55)
+
+            deny_button = Button(root, text="Deny", command=deny_order)
+            deny_button.place(anchor=W, x=150,y=80)
+
+            appendtolist([selections_menu, accept_button, view_button, deny_button])
+
+            box = Text(root, height = 4, width=15)
+            box.place(anchor=W, x=5, rely=.75)
+        except:
+            box = Text(root, height = 5, width=15)
+            box.insert(END, "No orders yet.")
+            box.place(anchor=W, x=5, rely=.6)
+
+        appendtolist([view_selector_label, box, back])
 
 def clear_root():
     for item in current_objs:
